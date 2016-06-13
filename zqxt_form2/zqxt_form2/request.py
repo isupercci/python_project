84c01dcf7258ae053ad8d4816f6fb2e6854e8396
Neil Chen <Neil Chen>
2016-06-14 00:10:35 +0800

    Test

---
+++ zqxt_form2/zqxt_form2/request.py
@@ -0,0 +1,6 @@
+import requests
+#my first request121212
+url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET'
+r = requests.get(url.replace('APPID', 'wx0287825317d8052e')
+                    .replace('APPSECRET', '52714a5e843d7bfbe295b5e5467d9c07'))
+print(r.text)
